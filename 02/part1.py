0ef0a082c0b305455a7b737e3d6558d07460d175
Mark <113889912+kno20205@users.noreply.github.com>
2024-12-02 10:09:16 +0100

    rm prints

--- 02/part1.py
+++ 02/part1.py
@@ -5,7 +5,6 @@ with open(file_path, 'r') as file:
     for line in file:
         is_row_safe = True
         row = [int(item) for item in line.strip().split()]
-        print(row)
         if row == sorted(row) or row == sorted(row, reverse=True):
             for i in range(len(row)):
                 if i > 0:
@@ -17,7 +16,6 @@ with open(file_path, 'r') as file:
         else:
             is_row_safe = False
         if is_row_safe:
-            print('Row is safe')
             safe += 1
 
 print(safe)
